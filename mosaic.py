50a559aef57295d16d74239336984c2bf60e7cab
federico.murphy <federico.murphy@teck.com>
2022-12-28 18:29:46 -0300

    Stop storing the initial image to free up memory. Fix crop image function

--- mosaic.py
+++ mosaic.py
@@ -21,23 +21,32 @@ def resize_image_by_factor(image, enlargement):
 
 
 def fit_image_to_tile(image, new_size, fit_method_name):
-    fit_methods = {"resize": resize_image_by_size, "crop": get_image_center}
+    fit_methods = {"resize": stretch_image_to_size, "crop": crop_image_to_size}
     fit_method = fit_methods[fit_method_name]
     return fit_method(image, new_size)
 
 
-def resize_image_by_size(image, new_size):
+def stretch_image_to_size(image, new_size):
     return image.resize(new_size)
 
 
-def get_image_center(image, new_size):
-    width_original, height_original = image.size
+def resize_image_mantain_ratio(image, proposed_size):
+    w, h = proposed_size
+    W, H = image.size
+    resize_factor = max(w/W, h/H)
+    final_size = (int(W * resize_factor), int(H * resize_factor))
+    return image.resize(final_size)
+
+
+def crop_image_to_size(image, new_size):
+    final_image = resize_image_mantain_ratio(image, new_size)
+    width_original, height_original = final_image.size
     width_final, height_final = new_size
 
     top_left_point_coordinates = ((width_original - width_final) / 2, (height_original - height_final) / 2)
     bottom_right_point_coordinates = ((width_original + width_final) / 2, (height_original + height_final) / 2)
     coordinates = top_left_point_coordinates + bottom_right_point_coordinates
-    return image.crop(coordinates)
+    return final_image.crop(coordinates)
 
 
 def get_average_rgb_from_image(image):
@@ -49,29 +58,31 @@ def get_average_rgb_from_image(image):
 class Mosaic:
     PICTURE_MODE = 'RGB'
 
-    def __init__(self, input_photo_path, other_photos_path, enlargement, tile_size):
+    def __init__(self, input_photo_path, other_photos_path, tile_size, output_file, enlargement=1, fit_method="resize"):
 
-        self.enlargement = enlargement
         self.image_coordinates_in_order = []
 
-        self.initial_image = self.process_initial_image(input_photo_path)
-        self.tile_size = tile_size
-        self.mosaic_tile_shape = self.get_total_number_of_tiles()
+        # Size of the tile image in pixels
+        self.tile_image_size = tile_size
 
+        # Read the target image, enlarge it, and
+        target_image = self.process_target_image(input_photo_path, enlargement)
+        self.mosaic_image_size = target_image.size  # Size of the final image in pixels
+        self.mosaic_image_shape = self.get_total_number_of_tiles()  # mosaic shape (number of tiles in rows and columns)
 
-        # Get the image split into many images
-        self.initial_image_split = []
-        self.subdivide_original_image()
+        total_tiles = self.mosaic_image_shape[0] * self.mosaic_image_shape[1]
+        color_size = 3
+        self.target_img_rgb_colors = np.zeros(shape=(total_tiles, color_size))
 
-        # Read the images and make them smaller
+        # Divide the target image and calculate the average color per tile
+        self.subdivide_original_image(target_image)
+
+        # Read the input images and make them smaller
         self.input_images = []
-        self.process_input_images(other_photos_path, fit_method_name="resize")
+        self.process_input_images(other_photos_path, fit_method_name=fit_method)
+        self.other_photos_rgb_colors = np.zeros(shape=(len(self.input_images), color_size))
 
         # Get the average color of each image
-        total_tiles = self.mosaic_tile_shape[0] * self.mosaic_tile_shape[1]
-        color_size = 3
-        self.input_photo_rgb_colors = np.zeros(shape=(total_tiles, color_size))
-        self.photo_list_rgb_colors = np.zeros(shape=(len(self.input_images), color_size))
         self.get_all_image_average_colors()
 
         # Using the distance between two points calculate
@@ -79,46 +90,48 @@ class Mosaic:
 
         # Create the mosaic
         self.mosaic_image = self.create_image_grid()
-        self.mosaic_image.save("output.png")
+        self.mosaic_image.save(output_file)
         print("Saved image successfully")
 
-
-    def process_initial_image(self, input_photo_path):
+    def process_target_image(self, input_photo_path, enlargement):
         img = load_image(input_photo_path)
-        final_img = resize_image_by_factor(img, self.enlargement)
-        print(f"Read and enlarged original image to {final_img.size}")
-        return final_img
+        img = resize_image_by_factor(img, enlargement)
+        self.mosaic_image_size = img.size
+        print(f"Upscale original image to {img.size}")
+        return img
 
     def get_total_number_of_tiles(self):
         # Get the width and height of the image
-        img_width, img_height = self.initial_image.size
+        img_width, img_height = self.mosaic_image_size
 
         # Calculate the number of rows and columns in the grid
-        cols = img_width // self.tile_size[0]
-        rows = img_height // self.tile_size[1]
+        cols = img_width // self.tile_image_size[0]
+        rows = img_height // self.tile_image_size[1]
         return rows, cols
 
-    def subdivide_original_image(self):
+    def subdivide_original_image(self, initial_image):
         # Get the tile size into parameters to make code more readable
-        tile_width, tile_height = self.tile_size
+        tile_width, tile_height = self.tile_image_size
 
         # Mosaic number of tiles
-        rows, cols = self.mosaic_tile_shape
+        rows, cols = self.mosaic_image_shape
 
         # Iterate over the rows and columns of the grid
-        row_cols_iterable = itertools.product(range(rows), range(cols))
-        for row, col in tqdm(row_cols_iterable, desc='Dividing Original Image'):
+        row_cols_iterable = enumerate(itertools.product(range(rows), range(cols)))
+
+        for k, (row, col) in tqdm(row_cols_iterable, desc='Dividing Original Image'):
             self.image_coordinates_in_order.append((row, col))
             top_left_point_coordinates = (col * tile_width, row * tile_height)
             bottom_right_point_coordinates = ((col + 1) * tile_width, (row + 1) * tile_height)
             coordinates = top_left_point_coordinates + bottom_right_point_coordinates
-            cropped_img = self.initial_image.crop(coordinates)
-            self.initial_image_split.append(cropped_img)
+            cropped_img = initial_image.crop(coordinates)
+            self.target_img_rgb_colors[k] = get_average_rgb_from_image(cropped_img)
+
 
     def process_input_images(self, images_directory, fit_method_name):
         img_list = self.get_images(images_directory)
         for image in tqdm(img_list, desc='Processing Input Images'):
-            self.input_images.append(fit_image_to_tile(image, self.tile_size, fit_method_name))
+            self.input_images.append(fit_image_to_tile(image, self.tile_image_size, fit_method_name))
 
     def get_images(self, images_directory):
         file_list = list(pathlib.Path(images_directory).glob('*[png|jpeg|jpg|PNG|JPEG|JPG]'))
@@ -132,17 +145,11 @@ class Mosaic:
     def get_all_image_average_colors(self):
         iterable = enumerate(self.input_images)
         for i, img in tqdm(iterable, desc='Getting average color for the base image'):
-            colors = get_average_rgb_from_image(img)
-            self.photo_list_rgb_colors[i] = colors
-
-        iterable = enumerate(self.initial_image_split)
-        for i, img in tqdm(iterable, desc='Getting average color of every input image'):
-            colors = get_average_rgb_from_image(img)
-            self.input_photo_rgb_colors[i] = colors
+            self.other_photos_rgb_colors[i] = get_average_rgb_from_image(img)
 
     def calculate_distances(self):
-        A = self.input_photo_rgb_colors
-        B = self.photo_list_rgb_colors
+        A = self.target_img_rgb_colors
+        B = self.other_photos_rgb_colors
 
         # Array A and B are 2D numpy arrays with shape (n, 3) and (m, 3), respectively
         # where n and m are the number of points in array A and array B, respectively
@@ -163,9 +170,9 @@ class Mosaic:
         return self.image_coordinates_in_order[i]
 
     def create_image_grid(self):
-        tile_width, tile_height = self.tile_size
+        tile_width, tile_height = self.tile_image_size
 
-        grid_img = Image.new("RGB", self.initial_image.size)
+        grid_img = Image.new("RGB", self.mosaic_image_size)
         iterable = range(len(self.indices_of_closest_image))
         for i in tqdm(iterable, desc='Creating Photo Mosaic Original Image'):
             row, col = self.oned_index_to_twod_index(i)
@@ -174,5 +181,11 @@ class Mosaic:
 
         return grid_img
 
+
 if __name__ == "__main__":
-    m = Mosaic("../input_image.png", "../C7 Photos", 5, (100, 100))
+    m = Mosaic(input_photo_path="../../sample_photos/Figu SIMU Astrid.png",
+               other_photos_path="../../sample_photos",
+               enlargement=10,
+               tile_size=(50, 50),
+               output_file="output.png",
+               fit_method="crop")
